addf8bc5806cb0d0c11ec14cadca4d9e554d006f
zgreathouse <greathouse.zachary@gmail.com>
2017-08-23 15:54:57 -0700

    initial commit- project setup: npm init, npm install node modules, create frontend folder, create stylesheets folder, create index.html file, create widgets.jsx entry file in front end folder, and create webpack config file

---
+++ frontend/widgets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//components
+import Clock from './clock';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<Root/>, document.getElementById('main'));
+});
